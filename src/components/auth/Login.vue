c59a02da36f5d3f447229f822f5bb2f9cb4ba16c
onevanivan <onevanivan1@gmail.com>
2021-03-13 03:35:33 +0200

    auth icons

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -7,9 +7,33 @@
         </div>
         <div class="form-item">
             <label class="form-item_label">Password</label>
-            <input class="form-item_input" type="text">
+            <div class="form-item_input-wrap">
+                <input class="form-item_input password" :type="passwordType">
+                <Eye class="form-item_icon"
+                     :class="{hidden:passwordType === 'password'}"
+                     @click="togglePasswordVisible()"/>
+            </div>
             <p class="auth_note">Don’t remember password?</p>
         </div>
-       <div class="btn-big auth_btn-login">Continue</div>
+        <div class="btn-big auth_btn-login">Continue</div>
     </div>
 </template>
+
+<script>
+    import Eye from '@/assets/img/eye.svg';
+
+    export default {
+        name: 'Login',
+        components: {
+            Eye
+        },
+        data: () => ({
+            passwordType: 'text'
+        }),
+        methods: {
+            togglePasswordVisible() {
+                this.passwordType = this.passwordType === 'password' ? 'text' : 'password';
+            }
+        }
+    };
+</script>
--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,20 @@ module.exports = {
     css: {
         loaderOptions: {
             scss: {
-                additionalData: `
-        @import "~@/assets/scss/variables.scss";
-      `,
+                additionalData: `@import "~@/assets/scss/variables.scss";`,
             },
         },
-    }
+    },
+    chainWebpack: (config) => {
+        const svgRule = config.module.rule('svg');
+
+        svgRule.uses.clear();
+
+        svgRule
+            .use('babel-loader')
+            .loader('babel-loader')
+            .end()
+            .use('vue-svg-loader')
+            .loader('vue-svg-loader');
+    },
 };
